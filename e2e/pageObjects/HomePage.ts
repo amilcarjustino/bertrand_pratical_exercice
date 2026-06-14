3cd8c39cccaa3cd2049a40178bf554ef7e833b52
amilcarjustino <amilcarjustino@gmail.com>
2026-06-15 00:37:41 +0100

    Add scenario 5

--- e2e/pageObjects/HomePage.ts
+++ e2e/pageObjects/HomePage.ts
@@ -1,8 +1,19 @@
-import { type Locator, type Page } from "@playwright/test";
+import { expect, type Locator, type Page } from "@playwright/test";
 
 const HOME_PAGE_URL = "/";
 const SEARCH_INPUT_NAME = "texto para pesquisa";
 const COOKIE_CONSENT_BUTTON_NAME = "Aceitar Todos";
+const HOME_PAGE_SECTION_HEADING_LEVEL = 2;
+
+const sectionsTitle = [
+    'Novidades a não perder',
+    'Pré-lançamentos',
+    'Os melhores descontos | até -',
+    'Os mais procurados online',
+    'Conexões Literárias | Temas',
+    'Os livros de que todos falam',
+    'Livros da semana com 50%',
+  ];
 
 export class HomePage {
   readonly page: Page;
@@ -19,8 +30,14 @@ export class HomePage {
     });
   }
 
+  // Dynamic locators for sections headings
+  async getSectionLocator(title: string) {
+    return this.page.getByRole("heading", { level: HOME_PAGE_SECTION_HEADING_LEVEL, name: title });
+  }
+
   async goto() {
     await this.page.goto(HOME_PAGE_URL);
+    await this.acceptCookieConsent();
   }
 
   async acceptCookieConsent() {
@@ -36,12 +53,18 @@ export class HomePage {
 
   async searchAndSelectTheBook(bookTitle: string) {
     await this.goto();
-    await this.acceptCookieConsent();
     await this.searchBook(bookTitle);
     await this.clickFirstBook(bookTitle);
   }
 
   async clickFirstBook(bookTitle: string) {
     await this.page.getByAltText(bookTitle).first().click();
+  }
+
+  async expectSectionsVisible() {
+    for (const title of sectionsTitle) {
+      const section = await this.getSectionLocator(title);
+      await expect(section).toBeVisible();
+    }
   }
 }
