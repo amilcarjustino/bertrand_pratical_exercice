cf17babdc02b6cdea61f5389382aebf8a3fce119
amilcarjustino <amilcarjustino@gmail.com>
2026-06-15 13:39:09 +0100

    Add screenshots for tests debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,10 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL ?? 'https://www.bertrand.pt/',
 
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
   timeout: 88_000,
 
